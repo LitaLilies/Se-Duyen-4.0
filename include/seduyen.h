c879212b8004d0915af67d5b871e1833bfef3464
trang050907-code <trang050907@gmail.com>
2026-04-23 21:18:21 +0700

    Add seduyen.h header file with Pair struct and functions

--- include/seduyen.h
+++ include/seduyen.h
@@ -1,1 +1,21 @@
-seduyen
+#ifndef SEDUYEN_H
+#define SEDUYEN_H
+
+#include "list.h"
+
+#define MAX_PAIR 500
+
+typedef struct {
+int id1;
+int id2;
+char name1 [MAX_NAME];
+char name2[MAX_NAME];
+float score;
+} Pair;
+
+float calcScore (Node *a, Node *b);
+void addPair(Node *head, int index1, int index2,
+Pair pairs, int *count);
+void sortPairs Desc(Pair pairs[], int count);
+
+#endif
